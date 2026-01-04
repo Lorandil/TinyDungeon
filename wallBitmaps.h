e622ea7333841e070844f54d9d8156b5a14e493a
Lorandil <70488869+Lorandil@users.noreply.github.com>
2026-01-04 17:00:45 +0100

    removed derelict DungeonMaster style wall bitmaps (which weren't functional anymore anyhow)

--- wallBitmaps.h
+++ wallBitmaps.h
@@ -2,12 +2,4 @@
 
 #include <Arduino.h>
 
-#define RUSTICAL_STYLE_WALLS
-//#define DUNGEON_MASTER_STYLE_WALLS
-
-#if defined( RUSTICAL_STYLE_WALLS )
-  #include "EyeOfTheBeholderWallBitmaps.h"
-#endif
-#if defined( DUNGEON_MASTER_STYLE_WALLS )
-  #include "DungeonMasterWallBitmaps.h"
-#endif
+#include "EyeOfTheBeholderWallBitmaps.h"
